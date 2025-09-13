f371470f8704d5f170e212ab176bdb597996e027
CameronBadman <cbadwork@gmail.com>
2025-09-13 21:33:12 +1000

    added web socket connection system and better sending function

--- frontend/src/Componants/Canvas/CanvasComponant.jsx
+++ frontend/src/Componants/Canvas/CanvasComponant.jsx
@@ -1,29 +1,75 @@
-
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import { Excalidraw } from '@excalidraw/excalidraw';
 import { debounce } from 'lodash';
+import process from 'process';
 
 const CanvasComponent = () => {
   const [elements, setElements] = useState([]);
   const [appState, setAppState] = useState({});
-
-  // Memoize the debounced function
-  const handleChange = useCallback(
-    debounce((newElements, newAppState, _files) => {
-      setElements(newElements);
-      setAppState(newAppState);
-    }, 300),
-    []
-  );
-
-  // Use useEffect to log the updates when the state changes
+  const previousElementsRef = useRef([]);
+  
+  const ws = useRef(null);
+  
   useEffect(() => {
-    console.log('Updated elements:', elements);
-    console.log('Updated appState:', appState);
-  }, [elements, appState]); // This effect runs whenever elements or appState change
+    ws.current = new WebSocket("ws://localhost:4000/123456789")
+    
+    ws.current.onopen = () => {}
+
+
+
+    ws.current.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data)
+        console.log(data)
+      } catch {
+        console.log("error failed to parse ws data")
+      }
+    }
+
+    ws.current.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+    
+    ws.current.onclose = () => {
+      console.log('WebSocket disconnected');
+    };
+
+    return () => {
+      if (ws.current) {
+        ws.current.close();
+      }
+    };
+
+  }, [])
+
+  const sendMessage = (message) => {
+    if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+      ws.current.send(message);
+    }
+  };
+
+  const handleChange = debounce((elements, appState) => {
+    const changedElements = elements.filter(element => {
+      const prevElement = previousElementsRef.current.find(prev => prev.id === element.id);
+      return !prevElement || prevElement.version !== element.version;
+    });
+    
+    const newElements = elements.filter(element => 
+      !previousElementsRef.current.find(prev => prev.id === element.id)
+    );
+
+    if (changedElements.length > 0 || newElements.length > 0) {
+    sendMessage(JSON.stringify(changedElements));
+    }
+
+    previousElementsRef.current = elements;
+  }, 100);
+
+  useEffect(() => {
+  }, [elements, appState]); 
 
   return (
-    <div style={{ width: '100%', height: '500px' }}>
+    <div style={{ width: '100%', height: '100%' }}>
       <Excalidraw
         initialData={{ elements, appState }}
         onChange={handleChange}
